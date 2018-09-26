5b74ada8de1269b2ddbc7ac740bf120f99f7efb4
dev19891112 <shinydaysyuru@gmail.com>
2018-09-27 00:37:59 +0900

    座標更新

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ var initDb = function(callback) {
   });
 };
 
-//app.use(express.static('views'));
+app.use(express.static('views'));
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
